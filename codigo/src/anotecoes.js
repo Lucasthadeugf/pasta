36873c49940679cb363bf47adb382e83ce45602b
Lucasthadeugf <lucasthadeufla@gmail.com>
2024-03-12 21:23:27 -0300

    primeiro commit

---
+++ codigo/src/anotecoes.js
@@ -0,0 +1,7 @@
+// Anotações para configurar um projeto typescript
+
+// npm install -D ts-node typescript
+        //para instalar as bibliotecas,  e o prejeto typescript
+
+// npx tsc --init
+        //para ele inicializar o projeto typescript
